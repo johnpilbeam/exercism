1975e69c15f307eee57afb37c17598fd8e59bf2c
johnpilbeam <jpilbeam@gmail.com>
2018-09-21 15:42:52 +0100

    Edited leap

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,13 +1,26 @@
-year = input("Enter year: ");
 
-    if (year % 4) == 0:
-        if (year % 100) == 0:
-            if (year % 400) == 0:
-                print("leap year")
-            else:
-                print("not a leap year")
-        else:
-            print("leap year")
+
+def is_leap_year(year):
+    if ((year % 4) == 0 and (year % 100) != 0):
+        print("leap year")
+    elif (year % 100) == 0:
+        print("leap year")
+    if (year % 400) == 0:
+            print("not a leap year")
     else:
-        print("not a leap year")
+        print("not a leap year")    
+            
+is_leap_year(2016)
+            
+# on every year that is evenly divisible by 4
+#   except every year that is evenly divisible by 100
+#     unless the year is also evenly divisible by 400   
+# -----------------------------------------------------#
+#                print("leap year")
+#           else:
+#                print("not a leap year")
+#        else:
+#            print("leap year")
+#    else:
+#        print("not a leap year")
 
